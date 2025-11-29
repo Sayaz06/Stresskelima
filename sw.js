c168df5f3360aa58ee8451e3bb935c871342e22b
Sayaz06 <adha86614@gmail.com>
2025-11-29 13:16:41 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "stresskelima-cache-v1";
+const CACHE_NAME = "stresskelima-cache-v3";
 const ASSETS = [
   "./",
   "./index.html",
@@ -36,3 +36,4 @@ self.addEventListener("fetch", (event) => {
     })
   );
 });
+
